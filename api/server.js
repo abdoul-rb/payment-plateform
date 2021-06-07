a962ab233dd4d2d055179b0697c96e7e7d2a4748
Come Wasik <wasik.come@gmail.com>
2021-06-07 11:35:01 +0200

    Add route file

--- api/server.js
+++ api/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
 
 // Set environment variables
 const PORT = process.env['WEB_PORT'] || 3000;
 
-app.get('/codes', (request, response) => {
-   console.log('Codes HTTP');
-});
-
+// Include routes
+const routes = require("./routes");
+app.use(routes);
 
 app.listen(PORT, () => console.log("Server listening"));
